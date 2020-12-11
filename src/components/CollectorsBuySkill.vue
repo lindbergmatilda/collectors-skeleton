a17c1f8adf1131df1b76187f4ef89c5c8a1141f4
kajsazw <kajsazw@gmail.com>
2020-12-11 15:00:02 +0100

    grid skill

--- src/components/CollectorsBuySkill.vue
+++ src/components/CollectorsBuySkill.vue
@@ -1,21 +1,26 @@
 <template>
 <div>
-  <h1>{{ labels.buySkill }}</h1>
-  <div class="buy-skill">
+
+  <center><h1>{{ labels.buySkill }}</h1></center>
+  <div class="item-left-right">
+
+  <div class="left buy-skill">
     <div v-for="(card, index) in skillsOnSale" :key="index">
       <CollectorsCard :card="card" :availableAction="card.available" @doAction="buySkill(card)" />
     </div>
   </div>
-  <div>
-    <div class="buttons" v-for="(p, index) in placement" :key="index">
-      <button v-if="p.playerId===null" :disabled="cannotAfford(p.cost)" @click="placeBottle(p)">
+
+    <div class="right buttons">
+      <div v-for="(p, index) in placement" :key="index">
+      <button class="button" v-if="p.playerId===null" :disabled="cannotAfford(p.cost)" @click="placeBottle(p)">
         ${{p.cost}}
       </button>
       <div v-if="p.playerId !== null">
         {{p.playerId}}
       </div>
-    </div>
-  </div>
+</div>
+</div>
+</div>
 </div>
 </template>
 
@@ -72,15 +77,46 @@ export default {
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.buy-skill {
+
+.item-left-right {
   display: grid;
-  grid-template-columns: repeat(auto-fill, 130px);
-  transform: scale(0.4)translate(-25%, 0);
+  grid-template-areas: 'left right';
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr;
+
+
 }
 .buttons {
-  height: 50px;
-  width: 500px;
+  width: 70px;
+
   display: grid;
+  grid-template-rows: repeat(auto-fill, 90px);
+
+
 }
+
+.button {
+  width: 50px;
+  height: 50px;
+}
+.left {
+grid-area: left;
+
+
+}
+
+.right {
+  grid-area: right;
+
+
+}
+.buy-skill {
+  display: grid;
+  grid-template-rows: repeat(auto-fill, 130px);
+  transform: scale(0.4)translate(-25%, 0);
+  transform-origin: top;
+}
+
+
 
 </style>

5fdef023e87a5ce4b2f20551e4e8e59ef7797aa3
nilssondisa <Disa.Nilsson.2415@student.uu.se>
2020-12-15 12:15:19 +0100

    Framsidan fixad med grids

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,58 +1,61 @@
 <template>
-
-  <div class="center">
-    <div>
-
-<h1>Collectors</h1>
-
+<div class="center">
+  <div>
+    <div class="upper-grid">
+      <h1>COLLECTORS</h1>
+    </div>
+    <div class="lower-grid">
       <div id="holder">
 
-      <div class="button" @click="setupCollectors(2, 'en')">
-          <p class="btnText">2 PLAYERS</p>
-          <div class="btnTwo">
-            <p class="btnText2">GO!</p>
+        <div class="left">
+          <div class="button" @click="setupCollectors(2, 'en')">
+            <p class="btnText">2 PLAYERS</p>
+            <div class="btnTwo">
+              <p class="btnText2">GO!</p>
+            </div>
           </div>
-       </div>
 
-      <div class="button" @click="setupCollectors(3, 'en')">
-          <p class="btnText">3 PLAYERS</p>
-          <div class="btnTwo">
-            <p class="btnText2">GO!</p>
+          <div class="button" @click="setupCollectors(3, 'en')">
+            <p class="btnText">3 PLAYERS</p>
+            <div class="btnTwo">
+              <p class="btnText2">GO!</p>
+            </div>
           </div>
-       </div>
 
-       <div class="button" @click="setupCollectors(4, 'en')">
-           <p class="btnText">4 PLAYERS</p>
-           <div class="btnTwo">
-             <p class="btnText2">GO!</p>
-           </div>
+          <div class="button" @click="setupCollectors(4, 'en')">
+            <p class="btnText">4 PLAYERS</p>
+            <div class="btnTwo">
+              <p class="btnText2">GO!</p>
+            </div>
+          </div>
+
         </div>
-
       </div>
 
       <div id="holder">
-
-      <div class="button" @click="setupCollectors(2, 'se')">
-          <p class="btnText">2 SPELARE</p>
-          <div class="btnTwo">
-            <p class="btnText2">KÖR!</p>
+        <div class="right">
+          <div class="button" @click="setupCollectors(2, 'se')">
+            <p class="btnText">2 SPELARE</p>
+            <div class="btnTwo">
+              <p class="btnText2">KÖR!</p>
+            </div>
           </div>
-       </div>
 
-      <div class="button" @click="setupCollectors(3, 'se')">
-          <p class="btnText">3 SPELARE</p>
-          <div class="btnTwo">
-            <p class="btnText2">KÖR!</p>
+          <div class="button" @click="setupCollectors(3, 'se')">
+            <p class="btnText">3 SPELARE</p>
+            <div class="btnTwo">
+              <p class="btnText2">KÖR!</p>
+            </div>
           </div>
-       </div>
 
-       <div class="button" @click="setupCollectors(4, 'se')">
-           <p class="btnText">4 SPLEARE</p>
-           <div class="btnTwo">
-             <p class="btnText2">KÖR!</p>
-           </div>
+          <div class="button" @click="setupCollectors(4, 'se')">
+            <p class="btnText">4 SPELARE</p>
+            <div class="btnTwo">
+              <p class="btnText2">KÖR!</p>
+            </div>
+          </div>
+
         </div>
-
       </div>
 
 
@@ -60,6 +63,7 @@
 
     </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -67,14 +71,16 @@
 
 export default {
   name: 'Home',
-  created: function () {
+  created: function() {
     this.$store.commit('SET_ROOM_ID');
   },
   methods: {
-    setupCollectors: function (playerCount, lang="en") {
-      this.$store.commit('SETUP_GAME', {roomId: this.$store.state.roomId,
+    setupCollectors: function(playerCount, lang = "en") {
+      this.$store.commit('SETUP_GAME', {
+        roomId: this.$store.state.roomId,
         playerCount: playerCount,
-        lang: lang });
+        lang: lang
+      });
       this.$router.push("/room/" + this.$store.state.roomId);
     },
   }
@@ -82,18 +88,25 @@ export default {
 </script>
 <style scoped>
 @import url(https://fonts.googleapis.com/css?family=Roboto:700);
-html, body{background : #FBC02D; font-family : Roboto; height: 100%; width: 100%; margin: 0;}
 
-  .center {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    padding: 1em;
+html,
+body {
+  background: #FBC02D;
+  height: 100%;
+  width: 100%;
+  margin: 0;
+}
 
-  }
+.center {
+  display: grid;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  grid-template-areas: 'up''down';
 
-  #holder{
+}
+
+#holder {
   -moz-user-select: -moz-none;
   -khtml-user-select: none;
   -webkit-user-select: none;
@@ -104,51 +117,85 @@ html, body{background : #FBC02D; font-family : Roboto; height: 100%; width: 100%
   position: relative;
   top: 50%;
   transform: translateY(-65%);
-  box-shadow: 0px 3px 8px rgba(0,0,0,0.25);
+  box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.25);
   border-radius: 2px;
   display: inline-block;
 
 }
+
 .button {
   background: #3D4C53;
-  margin : 20px auto;
-  width : 200px;
-  height : 50px;
+  margin: 20px auto;
+  width: 200px;
+  height: 50px;
   overflow: hidden;
-  text-align : center;
-  transition : .2s;
-  cursor : pointer;
+  text-align: center;
+  transition: .2s;
+  font-family: "Lexend Deca", sans-serif;
+  cursor: pointer;
   border-radius: 3px;
-  box-shadow: 0px 1px 2px rgba(0,0,0,.2);
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, .2);
 }
+
 .btnTwo {
-  position : relative;
-  width : 200px;
-  height : 100px;
+  position: relative;
+  width: 200px;
+  height: 100px;
   margin-top: -100px;
   padding-top: 2px;
-  background : #26A69A;
-  left : -250px;
-  transition : .3s;
+  background: #26A69A;
+  left: -250px;
+  transition: .3s;
 }
+
 .btnText {
-  color : white;
-  transition : .3s;
+  color: white;
+  transition: .3s;
 }
+
 .btnText2 {
-  margin-top : 63px;
-  margin-right : -130px;
-  color : #FFF;
+  margin-top: 63px;
+  margin-right: -130px;
+  color: #FFF;
 }
-.button:hover .btnTwo{ /*When hovering over .button change .btnTwo*/
+
+.button:hover .btnTwo {
+  /*When hovering over .button change .btnTwo*/
   left: -130px;
 }
-.button:hover .btnText{ /*When hovering over .button change .btnText*/
-  margin-left : 65px;
+
+.button:hover .btnText {
+  /*When hovering over .button change .btnText*/
+  margin-left: 65px;
 }
-.button:active { /*Clicked and held*/
-  box-shadow: 0px 5px 6px rgba(0,0,0,0.3);
+
+.button:active {
+  /*Clicked and held*/
+  box-shadow: 0px 5px 6px rgba(0, 0, 0, 0.3);
+}
+
+.upper-grid {
+  grid-area: up;
+  padding:100px;
+  padding-left: 192px;
+  font-family: "Lexend Deca", sans-serif;
+  font-size: 30px;
+  color: white;
 }
 
 
+.lower-grid {
+  display: grid;
+  grid-area:down;
+  grid-template-areas: 'left right';
+  grid-template-columns: 400px 400px;
+}
+
+.left {
+  grid-area: left;
+}
+
+.right {
+  grid-area: right;
+}
 </style>

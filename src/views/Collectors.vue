db06aff7edccb85331e89a1483db1c2573f01fa6
nilssondisa <Disa.Nilsson.2415@student.uu.se>
2020-12-15 16:38:57 +0100

    fixat auction knappar

--- src/views/Collectors.vue
+++ src/views/Collectors.vue
@@ -758,7 +758,7 @@ footer a:visited {
 
 
   grid-template-columns: 380px 2fr 400px;
-  grid-template-rows: 300px 1000px 1fr 1fr 1fr;
+  grid-template-rows: 300px 1200px 1fr 1fr 1fr;
 
   height: 500px;
   margin: 60px;

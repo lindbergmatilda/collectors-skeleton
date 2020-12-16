58af477625df75ad7e1cf0cb13a2a027d46d84d6
saramoren <sara-moren@hotmail.com>
2020-12-16 16:14:54 +0100

    v-if

--- src/views/Collectors.vue
+++ src/views/Collectors.vue
@@ -161,7 +161,7 @@
                   <h5 v-if="players[playerId]">{{ labels.placeBid }} </h5>
                   <h5 v-if="players[playerId] && this.auctionRunning">{{ labels.maxBid }} {{players[playerId].moneyCard}}</h5>
                 </div>
-                <input type="number" v-model="myBid" name="bid" :min="recentBid()" :max="playerMoney()" placeholder="Place your bid">
+                <input v-if="players[playerId]" type="number" v-model="myBid" name="bid" :min="recentBid()" :max="playerMoney()" placeholder="Place your bid">
                 <div class="altbuttons2">
                   <button class="altbutton2" v-if="players[playerId]" :disabled="!isMyAuctionTurn() || winnerAuction() || canNotAfford()" @click="placeBid()">BID</button>
                   <button class="altbutton2" v-if="players[playerId]" :disabled="!isMyAuctionTurn() || winnerAuction()" @click="passBid()">PASS</button>

4b090bdb709d879017bb0b6e6e5a1c089c66a876
lindbergmatilda <matilda.lindberg97@hotmail.com>
2020-12-18 12:10:43 +0100

    ser vem som börjar

--- src/views/Collectors.vue
+++ src/views/Collectors.vue
@@ -389,6 +389,7 @@ export default {
     this.$store.state.socket.on('collectorsClaimedFirstPlayer',
       function(d) {
         this.players = d;
+        this.isPlaying = this.whoIsPlaying();
       }.bind(this)
     );
 

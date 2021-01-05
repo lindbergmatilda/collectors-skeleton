bf87b613a4404d76c5379bdc69a5ee88732aba55
saramoren <sara-moren@hotmail.com>
2021-01-05 10:41:47 +0100

    winner

--- src/views/Collectors.vue
+++ src/views/Collectors.vue
@@ -26,7 +26,7 @@
 
 <div class="theWinner" id="theWinner">Find out who won</div>
 <button class="winner" id="winner" @click="winner">WINNER</button>
-<div v-if="theWinner" class="whoWon" id="whoWon"> GRATTIS {{theWinner.name}}</div>
+<div v-if="theWinner" class="whoWon" id="whoWon"> GRATTIS {{theWinner.name}} <img src="/images/trophy.jpg" width='500em'> </div>
 <div id="overlay"></div>
 
 
@@ -1490,13 +1490,16 @@ button[disabled] {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  border: 0.125em solid black; /* 2/16 */
-  border-radius: 0.625em; /* 10/16 */
+  border: 0.125em solid gold; /* 2/16 */
+  border-radius: 0.5em; /* 10/16 */
   z-index: 10;
   background-color: white;
   font-size: 6.25em; /* 100/16 */
+  width: 12em; /* 400/16 */
+  height: 4.5em; /* 250/16 */
   max-width: 80%;
   font-weight: bold;
+  text-align: center;
 }
 
 #overlay {
@@ -1518,10 +1521,11 @@ button[disabled] {
   left: 50%;
   transform: translate(-50%, -50%);
   border: 0.125em solid black; /* 2/16 */
+  border-radius: 0.5em; /* 10/16 */
   z-index: 10;
   background-color: gold;
-  width: 6.25em; /* 100/16 */
-  height: 6.25em; /* 100/16 */
+  width: 8em; /* 100/16 */
+  height: 8em; /* 100/16 */
   color: black;
   font-family: "Lexend Deca", sans-serif;
   font-size: 1.25em; /* 20/16 */
@@ -1534,12 +1538,12 @@ button[disabled] {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  border: 0.125em solid black; /* 2/16 */
+  border: 0.125em solid gold; /* 2/16 */
   border-radius: 0.625em; /* 10/16 */
   z-index: 10;
-  background-color: gold;
+  background-color: white;
   width: 12em; /* 400/16 */
-  height: 2em; /* 250/16 */
+  height: 6em; /* 250/16 */
   color: black;
   font-family: "Lexend Deca", sans-serif;
   font-size: 5em; /* 80/16 */
